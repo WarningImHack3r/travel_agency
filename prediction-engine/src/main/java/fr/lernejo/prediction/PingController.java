b5b7e04b7e5dfd390a2a915925c7ec20ad1c3c7a
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2021-12-09 14:36:09 +0100

    Tiny change

--- prediction-engine/src/main/java/fr/lernejo/prediction/PingController.java
+++ prediction-engine/src/main/java/fr/lernejo/prediction/PingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PingController {
 
     @GetMapping(path = "/api/ping")
-    String ping() {
+    public String ping() {
         return "OK";
     }
 }
